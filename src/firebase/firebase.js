d05942cdcbed77080de494e897b6057caee527b8
Clesoud <christopherjbarclay@gmail.com>
2018-11-17 16:56:53 -0800

    Error upon refresh of expense item: GET http://localhost:8080/edit/dist/bundle.js net::ERR_ABORTED 404 (Not Found)

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,8 +9,9 @@ const config = {
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  const database = firebase.database();
+const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-  export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
